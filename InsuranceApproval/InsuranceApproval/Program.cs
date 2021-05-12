06f883b2fde85614da73e36f527688e695676aef
Keen L <meisterkeen@gmail.com>
2021-05-12 13:36:23 -0700

    add boolean logic demo

---
+++ InsuranceApproval/InsuranceApproval/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InsuranceApproval
+{
+    class Program
+    {
+        static void Main()
+        {
+            // intro:
+            Console.WriteLine("Car Insurance Approval Questionnaire\n\nPlease Press Enter To Begin");
+            
+            // Start with getting user input:
+            Console.WriteLine("\nEnter your age in years (please enter a number):");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nHave you ever had a DUI? (Please enter 'True' or 'False')");
+            bool userDUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("\nHow many speeding tickets do you have? (please enter a number)");
+            int userTickets = Convert.ToInt32(Console.ReadLine());
+
+            // Crunchy logic!
+            // first: is the applicant of age?
+            bool validAge = userAge >= 15;
+            // no DUIs?
+            bool validDUI = userDUI == false;
+            // No more than 3 speeding tickets
+            bool validTickets = userTickets < 4;
+
+            if (validAge && validDUI && validTickets) 
+            {
+                Console.WriteLine("\n\nCongratulations! You are qualified for car insurance.");
+            }
+            else 
+            {
+                Console.WriteLine("\n\nUnfortunately you may not be qualified for our standard insurance services." +
+                    "\nPlease speak to an adjustor to review your qualifications and determine your options.");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
--- Tomfoolery/Tomfoolery/Program.cs
+++ Tomfoolery/Tomfoolery/Program.cs
@@ -53,43 +53,46 @@ namespace Tomfoolery
             //Console.WriteLine(isWarm);
             //Console.ReadLine();
 
-            // Console app assignment:
-            
-            // first we're going to prompt the user for input:
-            Console.WriteLine("Let's do some math! Please Enter a Number:");
-            // We need the user's input to convert to a long integer. This is where the failure may happen.
-            long inputNumber1 = Convert.ToInt64(Console.ReadLine());
-            // We'll add 25 to the input,
-            long result1 = inputNumber1 * 50;
-            // and output a string that tells the user what we did:
-            Console.WriteLine(inputNumber1 + " multiplied by 50 is: " + result1);
-            Console.ReadLine(); // pause for the user
+            //// Console app assignment:
 
-            // Now we'll duplicate this code for some other operations:
-            Console.WriteLine("This time we'll add 25 to your number:");
-            int inputNumber2 = Convert.ToInt32(Console.ReadLine());
-            int result2 = inputNumber2 + 25;
-            Console.WriteLine(inputNumber2 + " plus 25 is: " + result2);
-            Console.ReadLine();
+            //// first we're going to prompt the user for input:
+            //Console.WriteLine("Let's do some math! Please Enter a Number:");
+            //// We need the user's input to convert to a long integer. This is where the failure may happen.
+            //long inputNumber1 = Convert.ToInt64(Console.ReadLine());
+            //// We'll add 25 to the input,
+            //long result1 = inputNumber1 * 50;
+            //// and output a string that tells the user what we did:
+            //Console.WriteLine(inputNumber1 + " multiplied by 50 is: " + result1);
+            //Console.ReadLine(); // pause for the user
 
-            Console.WriteLine("input a number, which will be divided by 12.5:");
-            // I expect some floaty results so we're using doubles here
-            double inputNumber3 = Convert.ToDouble(Console.ReadLine());
-            double result3 = inputNumber3 / 12.5;
-            Console.WriteLine(inputNumber3 + " divided by 12.5 is: " + result3);
-            Console.ReadLine();
+            //// Now we'll duplicate this code for some other operations:
+            //Console.WriteLine("This time we'll add 25 to your number:");
+            //int inputNumber2 = Convert.ToInt32(Console.ReadLine());
+            //int result2 = inputNumber2 + 25;
+            //Console.WriteLine(inputNumber2 + " plus 25 is: " + result2);
+            //Console.ReadLine();
 
-            Console.WriteLine("input a number, and we'll check if it's greater than 50:");
-            // integers should be fine here
-            int inputNumber4 = Convert.ToInt32(Console.ReadLine());
-            bool result4 = inputNumber4 > 50;
-            Console.WriteLine("is " + inputNumber4 + " greater than 50? Answer: " + result4);
-            Console.ReadLine();
+            //Console.WriteLine("input a number, which will be divided by 12.5:");
+            //// I expect some floaty results so we're using doubles here
+            //double inputNumber3 = Convert.ToDouble(Console.ReadLine());
+            //double result3 = inputNumber3 / 12.5;
+            //Console.WriteLine(inputNumber3 + " divided by 12.5 is: " + result3);
+            //Console.ReadLine();
 
-            Console.WriteLine("we'll divide this one by 7, and show you the remainder:");
-            double inputNumber5 = Convert.ToDouble(Console.ReadLine());
-            double result5 = inputNumber5 % 7;
-            Console.WriteLine(inputNumber5 + " divided by 7, remainder is: " + result5);
+            //Console.WriteLine("input a number, and we'll check if it's greater than 50:");
+            //// integers should be fine here
+            //int inputNumber4 = Convert.ToInt32(Console.ReadLine());
+            //bool result4 = inputNumber4 > 50;
+            //Console.WriteLine("is " + inputNumber4 + " greater than 50? Answer: " + result4);
+            //Console.ReadLine();
+
+            //Console.WriteLine("we'll divide this one by 7, and show you the remainder:");
+            //double inputNumber5 = Convert.ToDouble(Console.ReadLine());
+            //double result5 = inputNumber5 % 7;
+            //Console.WriteLine(inputNumber5 + " divided by 7, remainder is: " + result5);
+            //Console.ReadLine();
+
+            Console.WriteLine(true != false);
             Console.ReadLine();
         }
     }
