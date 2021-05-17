c554d9763bf0c8ee0a1581f7bab27b1663d87103
Keen L <meisterkeen@gmail.com>
2021-05-17 14:22:02 -0700

    Add branching demo

---
+++ BranchingDemo/BranchingDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace BranchingDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1. Do a boolean comparison using a while statement:
+
+            // Ok, I'm going to try to do this in the simplest, cleanest way I can think of...
+
+            bool isTrue = true;
+
+            while (isTrue)
+            {
+                Console.WriteLine("It's true!");
+                break;
+            }
+            // Lol!! All I did was write a very clunky "if" statement using a while loop.
+            // Still technically works.
+
+
+            // 2. Do a boolean comparison using a do while statement:
+            do
+            {
+                Console.WriteLine("It's still true!");
+                break;
+            }
+            while (isTrue);
+            // Behold! An EVEN CLUNKIER "if" statement!
+
+            // just to make sure this do/while loop is behaving the way I think it is:
+
+            do
+            {
+                Console.WriteLine("It's not true!");
+                break;
+            }
+            while (!isTrue);
+            // WAIT, WHAT? isTrue is true, the "while()" statement is supposed to control the bit
+            // inside the "do" block, right? But it printed "It's not true" -- WHY?
+            // oh wait
+            // right
+            // it's because "do" makes the program execute the loop at least once, no matter
+            // what the "while()" statement is saying. So...
+
+
+            do
+            {
+                Console.WriteLine("Yes it is, it's TOTALLY true");
+                isTrue = false;
+                break;
+            }
+            while (!isTrue);
+
+            if (isTrue) 
+            {
+                Console.WriteLine("If you see this line, something is wrong");
+            }
+
+
+            // Fun thought: how many unneccesary while loops could be replaced with
+            // "if/else" branches? Are those less expensive than loops? Seems like
+            // while loops have more potential to break. And for me, if/else branches
+            // are way easier to think with.
+            // How many ways are there to skin this cat?
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- TernaryOps/TernaryOps/Program.cs
+++ TernaryOps/TernaryOps/Program.cs
@@ -63,13 +63,51 @@ namespace TernaryOps
             //Console.WriteLine(comparisonResult);
             //Console.ReadLine();
 
-            Console.WriteLine("what is your favorite number?");
-            int favNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("what is your favorite number?");
+            //int favNum = Convert.ToInt32(Console.ReadLine());
 
-            string result = favNum == 12 ? "Awesomesauce." : "You suck!";
+            //string result = favNum == 12 ? "Awesomesauce." : "You suck!";
 
-            Console.WriteLine(result);
-            Console.ReadLine();
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
         }
     }
 }
