3a1f0f8eaebecbfdfac8d052e40bb40968743a47
Keen L <meisterkeen@gmail.com>
2021-05-21 00:06:46 -0700

    add YetMoreMathClass

---
+++ YetMoreMathClass/YetMoreMathClass/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetMoreMathClass
+{
+    class Class1
+    { 
+        public void TwoInt(int num1, int num2)
+        {
+            // "void" meaning that this method doesn't expect to return anything anywhere.
+            int num3 = num1 * 7;
+            Console.WriteLine("The second argument you passed in was {0}", num2);
+
+
+        }
+    }
+}
---
+++ YetMoreMathClass/YetMoreMathClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetMoreMathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class:
+            Class1 class1 = new Class1();
+
+            // Call the method, passing in two arguments:
+            class1.TwoInt(5, 7);
+
+            // and that's it.
+            Console.ReadLine();
+
+            // I'm getting the hang of the flow of data, here. I think I can work with this.
+        }
+    }
+}
